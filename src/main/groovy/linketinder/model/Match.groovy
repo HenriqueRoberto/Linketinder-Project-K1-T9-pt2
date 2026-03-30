e553d329f332ded6907bb11409f720ae91b251c6
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-03-30 10:23:12 -0300

    feat(model): adiciona campo nome em Vaga e cria entidade Match

--- src/main/groovy/linketinder/model/Match.groovy
+++ src/main/groovy/linketinder/model/Match.groovy
@@ -1,4 +1,11 @@
 package linketinder.model
 
 class Match {
+    int idCandidato
+    int idVaga
+
+    Match(int idCandidato, int idVaga) {
+        this.idCandidato = idCandidato
+        this.idVaga = idVaga
+    }
 }
